1d18a4d8ed98241b3306a2bbf59ff9d1af56c974
JongReulKang <cheerjong12@naver.com>
2021-12-30 17:15:16 +0900

    카메라 앱으로 찍고 프로필사진으로 설정하기

--- app/src/main/java/com/tenriver/facecamera/MainActivity.kt
+++ app/src/main/java/com/tenriver/facecamera/MainActivity.kt
@@ -1,20 +1,156 @@
 package com.tenriver.facecamera
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
+import com.tenriver.facecamera.databinding.ActivityMainBinding
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     val REQUEST_IMAGE_CAPTURE = 1 // 카메라 사진 촬영 요청코드
     lateinit var curPhotoPath: String // 문자열 형태의 사진 경로 값 (초기 값을 null로 시작하고 싶을 때)
 
+    private var mBinding: ActivityMainBinding? = null // 전역 변수로 바인딩 객체 선언
+
+    private val binding get() = mBinding!! // 매번 null 체크 필요 없이 편의성을 위해 바인딩 변수 재 선언
+
+    private lateinit var getResult: ActivityResultLauncher<Intent>
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        
 
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        setPermission() // 권한을 체크하는 메소드 수행
+
+        binding.btnCamera.setOnClickListener {
+            takeCapture() // 기본 카메라 앱을 실행하여 사진 촬영
+        }
+
+        // 카메라 앱으로 부터 받아온 사진 결과 값
+        getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            // 이미지를 성공적으로 가져왔다면
+            if(it.resultCode == RESULT_OK) {
+                val bitmap: Bitmap
+                val file = File(curPhotoPath)
+                if(Build.VERSION.SDK_INT < 28 ){ // 안드로이드 9.0 (Pie) 버전보다 낮을 경우
+                    bitmap = MediaStore.Images.Media.getBitmap(contentResolver, Uri.fromFile(file))
+                    binding.ivProfile.setImageBitmap(bitmap)
+                } else{
+                    val decode = ImageDecoder.createSource(
+                        this.contentResolver,
+                        Uri.fromFile(file)
+                    )
+                    bitmap = ImageDecoder.decodeBitmap(decode)
+                    binding.ivProfile.setImageBitmap(bitmap)
+                }
+
+                savePhoto(bitmap)
+            }
+        }
 
     }
+
+    /**
+     * 갤러리에 저장
+     */
+    private fun savePhoto(bitmap: Bitmap) {
+        //val folderPath = Environment.getExternalStorageDirectory().absolutePath + "/Pictures/facecamera/" // 사진폴더로 저장하기 위한 경로 선언
+        val absolutePath =  "/storage/emulated/0/" // 사진폴더로 저장하기 위한 경로 선언
+        val folderPath = "${absolutePath}/Pictures/" // 사진폴더로 저장하기 위한 경로 선언
+        val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val fileName = "${timestamp}.jpeg"
+        val folder = File(folderPath)
+        if(!folder.isDirectory) { // 현재 해당 경로에 폴더가 존재하지 않는다면
+            folder.mkdirs() // make directory 줄임말로 해당 경로에 폴더 자동으로 새로 만들기
+        }
+
+        // 실제적인 저장 처리
+        val out = FileOutputStream(folderPath + fileName)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100,out)
+        Toast.makeText(this, "사진이 앨범에 저장되었습니다.",Toast.LENGTH_SHORT).show()
+    }
+
+    /**
+     * 카메라 촬영
+     */
+    private fun takeCapture() {
+        // 기본 카메라 앱 실행
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    null
+                }
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.tenriver.facecamera.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    getResult.launch(takePictureIntent)
+                    //startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE) //deprecated 됨
+                }
+            }
+        }
+    }
+
+    /**
+     * 이미지 파일 생성
+     */
+    private fun createImageFile(): File? {
+        val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile("JPEG_${timestamp}_",".jpg",storageDir)
+            .apply { curPhotoPath = absolutePath }
+
+    }
+
+    /**
+     * 테드 퍼미션 설정
+     */
+    private fun setPermission() {
+        val permission = object : PermissionListener {
+            override fun onPermissionGranted() { // 설정해놓은 위험권한들이 허용 되었을 경우 이 곳을 수행함
+                Toast.makeText(this@MainActivity, "권한이 허용 되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) { // 설정해놓은 위험권한 들 중 거부를 한 경우 이곳을 수행함
+                Toast.makeText(this@MainActivity, "권한이 거부 되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        TedPermission.with(this)
+            .setPermissionListener(permission)
+            .setRationaleMessage("카메라를 사용하시려면 권한을 허용해주세요.")
+            .setDeniedMessage("권한을 거부하셨습니다. [앱 설정] -> [권한] 항목에서 허용해주세요.")
+            .setPermissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.CAMERA)
+            .check()
+    }
+
+
+
 }
